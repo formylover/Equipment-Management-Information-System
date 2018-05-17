0499aef6d9749354476ef7a98b0315eaca6701b1
TuringLiu <liuliugai1@163.com>
2018-05-17 22:28:05 +0800

    装备信息管理系统-0.01
    第一个主版本的发布。

---
+++ 装备管理信息系统/装备管理信息系统/Form2.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace 装备管理信息系统
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(294, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "系统管理";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 119);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "基本信息管理";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(270, 119);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(161, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "装备库存管理";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(513, 119);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(157, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "装备维修与调拨管理";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(44, 216);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 40);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "装备经费信息管理";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(270, 216);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(161, 40);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "装备资料信息管理";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(513, 218);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(157, 38);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "统计查询管理";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 314);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.Text = "功能选择界面-装备信息管理系统";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
